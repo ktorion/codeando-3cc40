98094fa9ca12bb1e0977a3e1440d0eac31562d4d
Stackbit Code Editor <projects@stackbit.com>
2021-09-04 23:05:30 +0000

    Newsletter.jsx: changed

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -30,7 +30,7 @@ const Newsletter = () => {
   return (
     <div className="container py-12 lg:py-16">
       <h2 className="text-3xl sm:text-4xl font-extrabold text-gray-900 leading-tight tracking-tight">
-        inscribete 
+        unete a
       </h2>
       <form className="mt-8 sm:flex" onSubmit={handleSubmit}>
         <input
