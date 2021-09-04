2d27d7afd156eb74364bd6baf267afa18011bbf8
Stackbit Code Editor <projects@stackbit.com>
2021-09-04 22:42:20 +0000

    about.js: changed

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,7 +22,7 @@ const AboutPage = ({ data }) => (
             </h2>
             <div className="mt-4 leading-loose">
               me considero sencillo y pragmatico, aun asitengo un gran interes por el arte 
-              <br /> y el diseño
+              <br /> y el diseño.
               <br /> procuro mantener en mis proyectos y metodologias de estudio esos dos principios 
               
               <br /> estaría encantado de colaborar contigo
