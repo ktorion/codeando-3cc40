836c173f26f1529567269971638dbbf1d0e1e4a9
Stackbit Code Editor <projects@stackbit.com>
2021-09-05 00:00:24 +0000

    Header.jsx: changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,9 +22,9 @@ const Header = () => {
   return (
     <div className="container pt-6 pb-12 md:pt-12">
       <div className="flex justify-between items-center">
-        
+        <Link to="/">
           
-        
+        </Link>
 
         <button
           className="sm:hidden"
